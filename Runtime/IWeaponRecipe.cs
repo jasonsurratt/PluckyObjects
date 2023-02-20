0cfc42e88cf84c16aa7eb5764836c25f22815a71
Jason R. Surratt <jason.surratt@gmail.com>
2023-02-20 08:57:30 -0500

    Add many interfaces

---
+++ Runtime/IWeaponRecipe.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Plucky.Objects
+{
+    public interface IWeaponRecipe
+    {
+        IWeaponDefinition recipeMin { get; set; }
+        IWeaponDefinition recipeMax { get; set; }
+    }
+}
---
+++ Runtime/MaterialPicker.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Plucky.Objects
+{
+    public class MaterialPicker : MonoBehaviour
+    {
+        public Texture baseMap;
+        public Texture normalMap;
+        public float smoothness;
+
+
+    }
+}
